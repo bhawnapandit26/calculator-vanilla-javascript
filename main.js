1e5fab76cb7df8f0640b5009d92510fb9c50d2c0
Bhawna Pandit <142803539+bhawnapandit26@users.noreply.github.com>
2023-08-31 18:02:08 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,84 @@
+//gettig the nodelist of all theme selectors/ input fields
+const themeOptions = document.querySelectorAll("[name = 'theme']");
+
+//storing the theme locally
+function storeTheme (theme){
+    localStorage.setItem("theme", theme);
+}
+
+//when this local storing function will be called
+themeOptions.forEach((theme)=>{
+    theme.addEventListener('click', ()=>{
+        storeTheme(theme.id);
+    })
+});
+
+//theme value retrival function from local storage
+function retriveTheme() {
+    const selectedTheme = localStorage.getItem("theme");
+    //we need to check the button which is equal to the retrieved item
+    themeOptions.forEach((theme)=>{
+        if(theme.id === selectedTheme)
+        {
+            theme.checked = true;
+        }
+    })
+}
+
+//when this retrival function will be called
+document.onload = retriveTheme();
+
+
+//*************calculation part starts here******************
+
+let outputScreen = document.getElementById("output-screen");
+const button = document.querySelectorAll(".key");
+let result = [];
+
+//checking what type of button is clicked
+button.forEach((btn)=>{
+    btn.addEventListener('click', ()=>{
+        if(btn.classList.contains('num-key')){
+            result.push(btn.innerHTML);
+            outputScreen.value = result.join('');
+
+
+        }
+
+        if(btn.classList.contains('op-key')){
+            result.push(btn.innerHTML);
+            outputScreen.value = result.join('');
+
+
+        }
+
+        if(btn.classList.contains('eq-key')){
+            outputScreen.value = parseFloat(eval(result.join(''))).toFixed(2);
+
+
+        }
+
+        if(btn.classList.contains('del-key')){
+            result.pop();
+            if(result.length == 0)
+            {
+                outputScreen.value = "000";
+            }
+            else{
+                outputScreen.value = result.join('');
+            }
+           
+
+
+        }
+
+        if(btn.classList.contains('reset-key')){
+            result = [''];
+            outputScreen.value = "000";
+
+
+        }
+    });
+});
+
+
